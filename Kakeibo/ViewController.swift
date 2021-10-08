68d94243b6c65773c9af6ad81269909991d35b69
Daigo Kondo <daigo.soccer.0106@icloud.com>
2021-10-08 16:27:51 +0900

    Initial Commit

---
+++ Kakeibo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kakeibo
+//
+//  Created by 近藤大伍 on 2021/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
